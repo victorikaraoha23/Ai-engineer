c52826235b78186982b35ba924042169790049c8
victorikaraoha23 <victorikaraoha23@gmail.com>
2026-08-02 18:32:19 +0100

    started project 1.2

--- open-router mode comparison/model-comparisons.py
+++ open-router mode comparison/model-comparisons.py
@@ -6,8 +6,15 @@ from openai import OpenAI
 load_dotenv()
 
 client = OpenAI(base_url="https://openrouter.ai/api/v1")
-response = client.chat.completions.create(
-    model="openouter/free", message=[{"role": "user", "content": "define noun"}]
-)  
 
-print(response.choices[0].message.content)
+def openrouter_call():
+    model_list=["openrouter/free","~deepseek/deepseek-v4-flash-latest"]
+    for models in model_list:
+        response = client.chat.completions.create(
+            model=models,
+            messages=[{"role": "user", "content": sys.argv[1]}],
+        )
+        return response.choices[0].message.content
+
+
+print(openrouter_call())
